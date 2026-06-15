d4fb657e77b78d6f4c731f9b60c90c8274bf27ec
NouranG <nouranlahzy38@gmail.com>
2026-06-15 14:31:39 +0300

    adding embedding script

---
+++ Src/Embedding/embedding.py
@@ -0,0 +1,40 @@
+# embeddings.py
+
+import torch
+import torch.nn as nn
+
+class GPTEmbeddings(nn.Module):
+
+    def __init__(self,vocab_size,embed_dim,block_size):
+        super().__init__()
+
+        # Token embeddings
+        self.token_embedding = nn.Embedding(vocab_size,embed_dim)
+
+        # Positional embeddings
+        self.position_embedding = nn.Embedding(block_size, embed_dim)
+
+    def forward(self, x):
+        """
+        x shape:
+        (batch_size, seq_len)
+        """
+
+        batch_size, seq_len = x.shape
+
+        # Create positions:
+        # [0, 1, 2, ..., seq_len-1]
+        positions = torch.arange(
+            seq_len,
+            device=x.device
+        )
+
+        token_emb = self.token_embedding(x)
+
+        position_emb = self.position_embedding(
+            positions
+        )
+
+        embeddings = token_emb + position_emb
+
+        return embeddings
