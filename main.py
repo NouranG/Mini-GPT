08b901474e63ce402473430e694d351358c85027
NouranG <nouranlahzy38@gmail.com>
2026-06-27 19:41:43 +0300

    Gradio interface script

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from mini-gpt!")
+import gradio as gr
+
+from Src.inference.generate import generate_text
 
 
-if __name__ == "__main__":
-    main()
+demo = gr.Interface(
+    fn=generate_text,
+
+    inputs=[
+        gr.Textbox(
+            label="Prompt",
+            placeholder="Enter your prompt here...",
+            lines=5,
+        ),
+    ],
+
+    outputs=[
+        gr.Textbox(
+            label="Generated Text",
+            lines=15,
+        ),
+    ],
+
+    title="Mini GPT",
+
+    description="""
+Character-level GPT built completely from scratch using PyTorch.
+Enter a prompt and let the model continue generating text.
+""",
+)
+
+demo.launch()
