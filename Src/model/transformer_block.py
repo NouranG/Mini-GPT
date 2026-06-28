17b76c2aa73c053e988560a52e76250c9acade71
NouranG <nouranlahzy38@gmail.com>
2026-06-28 22:12:35 +0300

    editing transformer script

--- Src/model/transformer_block.py
+++ Src/model/transformer_block.py
@@ -11,7 +11,9 @@ class FeedForward(nn.Module):
         self.net = nn.Sequential(
             nn.Linear(embed_dim, 4 * embed_dim),
             nn.GELU(),
-            nn.Linear(4 * embed_dim, embed_dim)
+            nn.Linear(4 * embed_dim, embed_dim),
+                nn.Dropout(0.1)
+
         )
 
     def forward(self, x):
